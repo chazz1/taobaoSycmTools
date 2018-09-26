70e9588a2066f1bae5427df6a263097b4ce280c0
23 <3123@q.c>
2018-09-26 16:42:34 +0800

    测试

---
+++ taobaoSycmTools.user.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name         taobao sycm tools
+// @name:zh      淘宝生意参谋工具箱 chazz
+// @namespace    https://github.com/chazz1/taobaosycm
+// @version      0.0.1
+// @description  生意参谋商品效果助手，一键计算收藏加购率，一键根据设置选择需要的数据
+// @icon          https://img.alicdn.com/tps/i1/TB1.OB5HpXXXXbyXpXXFArBHXXX-48-48.ico
+// @author       chazz <chazzcfb@163.com>
+// @match        *://sycm.taobao.com/*
+// @require      https://cdn.bootcss.com/jquery/1.12.4/jquery.js
+// @require      https://cdn.bootcss.com/clipboard.js/2.0.1/clipboard.min.js
+// @require      https://raw.githubusercontent.com/chazz1/taobaoSycmTools/master/helper.js
+
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @connect     s.taobao.com
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    var pathname = window.location.pathname;
+    console.log(pathname);
+    if (pathname == "/bda/items/effect/item_effect.htm") {
+        var btn1 = '<div class="ui-switch btn-group-switch"><ul class="ui-switch-menu"><li class="ui-switch-item ui-routable-item ch-btn hidden"><a href="JavaScript:;">计算收藏加购率</a></li><li class="ui-switch-item ui-routable-item ch-btn2"><a href="JavaScript:;">看人气</a></li><li class="ui-switch-item ui-routable-item ch-btn3"><a href="JavaScript:;">看价值</a></li></ul></div>';
+        $(".operation").prepend(btn1);
+        /*商品效果 */
+        $("li.ch-btn2").click(function () {
+            if ($(".ch-btn").hasClass("hidden")) {
+                $(".ch-btn").removeClass("hidden");
+            }
+            gouxuan(4, 5, 20, 21);
+        });
+        $("li.ch-btn3").click(function () {
+            if (!$(".ch-btn").hasClass("hidden")) {
+                $(".ch-btn").addClass("hidden");
+            }
+            gouxuan(3, 10, 14, 15);
+        });
+        $("li.ch-btn").click(function () {
+            $("span.ch-span").remove()
+            jsShangPin();
+        });
+    }
+    if (pathname == "/bda/items/itemanaly/item_analy.htm") {
+        /*单品分析 */
+        var btn1 = '<li class="ui-switch-item ui-routable-item ch-btn"><a href="JavaScript:;">计算</a></li><li class="ui-switch-item ui-routable-item ch-btn2"><a href="JavaScript:;" class="ch-copy-btn">拆分词根</a></li>';
+        $(".btn-group-switch .ui-switch-menu").append(btn1);
+        
+        $("li.ch-btn").click(function () {
+            $("span.ch-span").remove()
+            jsDanPin();
+        });
+        $("li.ch-btn2").click(function () {
+            chaiFenBiaoTi();
+        });
+    }
+
+})();
+
+
+
